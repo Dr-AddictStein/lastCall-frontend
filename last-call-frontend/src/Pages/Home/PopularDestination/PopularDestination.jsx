173e8305cb8a395c2f6efbaefcb9d2fb9e2a991f
ESRAILHAQUE <esrailbblhs@gmail.com>
2024-06-15 11:54:05 +0600

    homepage card

--- last-call-frontend/src/Pages/Home/PopularDestination/PopularDestination.jsx
+++ last-call-frontend/src/Pages/Home/PopularDestination/PopularDestination.jsx
@@ -1,12 +1,12 @@
-import canbera from "../../../assets/images/Banner/Canberra.jpeg";
+import canbera from "../../../assets/images/Banner/Brisbane.jpg";
+
 
 function PopularDestination() {
   const destinations = [
     { id: 1, name: "Spain", image: canbera },
-    { id: 2, name: "Spain", image: canbera },
-    { id: 3, name: "Spain", image: canbera },
-    { id: 4, name: "Spain", image: canbera },
-   
+    { id: 2, name: "Australia", image: canbera },
+    { id: 3, name: "Japan", image: canbera },
+    { id: 4, name: "Thailand", image: canbera },
   ];
 
   return (
@@ -15,16 +15,21 @@ function PopularDestination() {
         <p className="my-6 text-2xl font-bold">Popular Destinations</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {destinations.map((destination) => (
-            <div key={destination.id}>
-              <div className="relative">
+            <div
+              key={destination.id}
+              className="relative group overflow-hidden rounded-lg"
+            >
+              <div className="relative w-full h-full">
                 <img
-                  className="rounded-lg w-full h-full object-cover"
+                  className="w-full h-full object-cover transition-transform duration-500 ease-in-out transform group-hover:scale-110"
                   src={destination.image}
                   alt={destination.name}
                 />
-                <p className="absolute bottom-2 left-1 bg-base-300 px-5 py-3 rounded-3xl">
-                  {destination.name}
-                </p>
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <p className="bg-black bg-opacity-20  p-4 text-white text-3xl  font-bold rounded">
+                    {destination.name}
+                  </p>
+                </div>
               </div>
             </div>
           ))}
--- last-call-frontend/src/Pages/Home/PopularHotels/PopularHotels.jsx
+++ last-call-frontend/src/Pages/Home/PopularHotels/PopularHotels.jsx
@@ -1,100 +1,41 @@
-import { MdKeyboardArrowRight } from 'react-icons/md';
-import img1 from '../../../assets/images/Banner/melbourne2.jpg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import img1 from "../../../assets/images/Banner/melbourne2.jpg";
+import { Link } from "react-router-dom";
+
 function PopularHotels() {
   return (
     <div className="mt-4 mb-20">
       <div>
         <p className="text-2xl font-bold my-4">Hotels loved by guests</p>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 lg:grid-cols-5'>
-        <div className="card  bg-base-100 shadow-xl">
-          <figure className="px-4 pt-10">
-            <img src={img1} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body  ">
-            <p className="font-semibold">Shoho Hotel London</p>
-            <p>London</p>
-            <p className="font-bold">$130/Night</p>
-            <div className="card-actions justify-end">
-              <Link>
-                <button className="btn ">
-                  <MdKeyboardArrowRight className="text-2xl" />
-                </button>
-              </Link>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 lg:grid-cols-5">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="card bg-base-100 shadow-xl group">
+            <figure className="px-4 pt-10 relative overflow-hidden">
+              <img
+                src={img1}
+                alt="Shoes"
+                className="rounded-xl w-full h-full obj  transition-transform duration-500 ease-in-out transform group-hover:scale-110"
+              />
+            </figure>
+            <div className="card-body">
+              <p className="font-semibold">Shoho Hotel London</p>
+              <p>London</p>
+              <p className="font-bold">$130/Night</p>
+              <div className=" flex justify-end">
+                <Link to="#">
+                  <button className=" ">
+                    <MdKeyboardArrowRight className="text-2xl" />
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card  bg-base-100 shadow-xl">
-          <figure className="px-4 pt-10">
-            <img src={img1} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body  ">
-            <p className="font-semibold">Shoho Hotel London</p>
-            <p>London</p>
-            <p className="font-bold">$130/Night</p>
-            <div className="card-actions justify-end">
-              <Link>
-                <button className="btn ">
-                  <MdKeyboardArrowRight className="text-2xl" />
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="card  bg-base-100 shadow-xl">
-          <figure className="px-4 pt-10">
-            <img src={img1} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body  ">
-            <p className="font-semibold">Shoho Hotel London</p>
-            <p>London</p>
-            <p className="font-bold">$130/Night</p>
-            <div className="card-actions justify-end">
-              <Link>
-                <button className="btn ">
-                  <MdKeyboardArrowRight className="text-2xl" />
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="card  bg-base-100 shadow-xl">
-          <figure className="px-4 pt-10">
-            <img src={img1} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body  ">
-            <p className="font-semibold">Shoho Hotel London</p>
-            <p>London</p>
-            <p className="font-bold">$130/Night</p>
-            <div className="card-actions justify-end">
-              <Link>
-                <button className="btn ">
-                  <MdKeyboardArrowRight className="text-2xl" />
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="card  bg-base-100 shadow-xl">
-          <figure className="px-4 pt-10">
-            <img src={img1} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body  ">
-            <p className="font-semibold">Shoho Hotel London</p>
-            <p>London</p>
-            <p className="font-bold">$130/Night</p>
-            <div className="card-actions justify-end">
-              <Link>
-                <button className="btn ">
-                  <MdKeyboardArrowRight className="text-2xl" />
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 }
-export default PopularHotels
+
+export default PopularHotels;
