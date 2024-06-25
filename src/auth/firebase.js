14c0a5c64a0190b65c95bd7b2ab4c33a74843ad9
Dr-AddictStein <dr.addictstein@gmail.com>
2024-06-25 19:25:24 +0600

    auth-done

---
+++ src/auth/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiNWPjxb8zYrBKUYd8-lAfgYhbh_BP4KA",
+  authDomain: "lastcall-194e0.firebaseapp.com",
+  projectId: "lastcall-194e0",
+  storageBucket: "lastcall-194e0.appspot.com",
+  messagingSenderId: "775523112330",
+  appId: "1:775523112330:web:3e95d946fd14dfa7552473"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
